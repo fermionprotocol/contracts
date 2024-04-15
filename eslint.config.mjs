9fc6e888804e33eb7b40c3f3a9dc6550b8d65d72
zajck <klemen@impressieve.com>
2024-04-15 12:59:31 +0200

    linter and prettifier config

---
+++ eslint.config.mjs
@@ -0,0 +1,20 @@
+import tseslint from "typescript-eslint";
+import eslintConfigPrettier from "eslint-config-prettier";
+
+export default [
+  ...tseslint.configs.recommended,
+  eslintConfigPrettier,  
+  {
+    languageOptions: {
+      sourceType: "module",
+      ecmaVersion: 2020,
+    },
+    rules: {
+      "no-empty": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+    },
+  },
+  {
+    ignores: ["**/libraries/**/*.ts","artifacts/**/*.ts","typechain-types/**"],
+}
+];
