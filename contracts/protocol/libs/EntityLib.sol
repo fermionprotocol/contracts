4956d5f62c57da0580cd4f191e727260a974f7a5
zajck <klemen@impressieve.com>
2024-07-17 16:27:48 +0200

    Remove non-standard method invocation

--- contracts/protocol/libs/EntityLib.sol
+++ contracts/protocol/libs/EntityLib.sol
@@ -35,7 +35,7 @@ library EntityLib {
         FermionStorage.ProtocolEntities storage pe = FermionStorage.protocolEntities();
         FermionTypes.EntityData storage newEntity = pe.entityData[entityId];
 
-        EntityLib.storeEntity(entityId, _admin, newEntity, _roles, _metadata);
+        storeEntity(entityId, _admin, newEntity, _roles, _metadata);
         storeCompactWalletRole(entityId, _admin, 0xff << (31 * BYTE_SIZE), true, pl, pe); // compact role for all current and potential future roles
         emitAdminWalletAddedOrRemoved(entityId, _admin, true);
     }
