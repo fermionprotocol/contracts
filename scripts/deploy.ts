2e8a470daa6447b402628b9b4a8c5fcab9690182
zajck <klemen@impressieve.com>
2025-02-14 08:33:10 +0100

    Update FNFT on amoy

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -120,6 +120,7 @@ export async function deploySuite(env: string = "", modules: string[] = [], crea
     wrapperImplementationAddress = await fermionWrapper.getAddress();
 
     deploymentComplete("FermionFNFT", wrapperImplementationAddress, fermionFNFTConstructorArgs, true);
+    await writeContracts(deploymentData, env, version, externalContracts);
   } else {
     deploymentData = await getDeploymentData(env);
     wrapperImplementationAddress = deploymentData.find((contract) => contract.name === "FermionFNFT")?.address;
