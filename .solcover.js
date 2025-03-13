4c6d6d32af3f47c19d89567d062d4c5edd540037
Klemen <64400885+zajck@users.noreply.github.com>
2025-03-13 17:58:43 +0100

    Add changes from main to current develop branch (#398)
    * Add audits to the repo (#285)
    
    * Add audits
    
    * Upload fixed v1.0.0_2 pdf
    
    * feat:implement name and symbol setters
    
    * feat: unit tests
    
    * v1.0.1 addresses (#279)
    
    * amoy and sepolia test addresses
    
    * update protocol parameters
    
    * amoy v1.0.1. staging addresses
    
    * Fix protocol versions in addresses files
    
    * Update parameters in tests
    
    * Make better gas cost estimates
    
    * add base sepolia parameters
    
    * add base testi and staging addresses
    
    * Add base config
    
    * Base v1.0.1 mainnet addresses
    
    * tidy
    
    * Better dry-run
    
    * automatically renounce UPGRADER role
    
    * Update FNFT on amoy
    
    * Polygon v1.0.1 mainnet addresses
    
    * Ethereum v1.0.1 mainnet addresses
    
    * update FNFT implementation address
    
    * tidy
    
    * Optimism v1.0.1 test,staging addresses
    
    * Ethereum v1.0.1 sepolia addresses
    
    * Update sepolia test FNFT implementation
    
    * Fix failing unit tests
    
    ---------
    
    Co-authored-by: 0xlucian <0xluciandev@gmail.com>
    
    * Verify contracts on block explorer (#318)
    
    * amoy and sepolia test addresses
    
    * update protocol parameters
    
    * amoy v1.0.1. staging addresses
    
    * Fix protocol versions in addresses files
    
    * Update parameters in tests
    
    * Make better gas cost estimates
    
    * Add verification script
    
    * Fix readme typos
    
    * add base sepolia parameters
    
    * add base testi and staging addresses
    
    * Add base config
    
    * Base v1.0.1 mainnet addresses
    
    * tidy
    
    * verify base contracts
    
    * baseSepolia hardhat config
    
    * Handle "InitializationFacet" verification
    
    * Better dry-run
    
    * automatically renounce UPGRADER role
    
    * Update FNFT on amoy
    
    * Polygon v1.0.1 mainnet addresses
    
    * Ethereum v1.0.1 mainnet addresses
    
    * update FNFT implementation address
    
    * tidy
    
    * Optimism v1.0.1 test,staging addresses
    
    * optimistic etherscan verification
    
    * Ethereum v1.0.1 sepolia addresses
    
    ---------
    
    Co-authored-by: 0xlucian <0xluciandev@gmail.com>
    
    * add banner
    
    * Add files via upload
    
    * add docs link
    
    * new banner
    
    * remove banner size
    
    * tidy
    
    * Add correct coverage badge (#397)
    
    * Merge branch 'main' into develop
    
    * fix tests
    
    * Seaport integration test (#327)
    
    * amoy and sepolia test addresses
    
    * update protocol parameters
    
    * amoy v1.0.1. staging addresses
    
    * Fix protocol versions in addresses files
    
    * Update parameters in tests
    
    * Make better gas cost estimates
    
    * use true boson and seport when doing dry-run
    
    * unwrap on forked network
    
    * rename ethereum -> mainnet, skip deployer check in dry run
    
    * skip integration test when running normal tests
    
    * Update readme
    
    * add base sepolia parameters
    
    * add base testi and staging addresses
    
    * Add base config
    
    * Add base, rename mainnet back to ethereum
    
    * Base v1.0.1 mainnet addresses
    
    * tidy
    
    * increase timeout
    
    * add optimism to readme
    
    * Update README.md
    
    Co-authored-by: albertfolch-redeemeum <102516373+albertfolch-redeemeum@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: albertfolch-redeemeum <102516373+albertfolch-redeemeum@users.noreply.github.com>
    
    * update package.json
    
    ---------
    
    Co-authored-by: 0xlucian <0xluciandev@gmail.com>
    Co-authored-by: 0xlucian <96285542+0xlucian@users.noreply.github.com>
    Co-authored-by: git-fermion <159157137+git-fermion@users.noreply.github.com>
    Co-authored-by: albertfolch-redeemeum <102516373+albertfolch-redeemeum@users.noreply.github.com>

--- .solcover.js
+++ .solcover.js
@@ -8,4 +8,8 @@ module.exports = {
   ],
   modifierWhitelist: ["nonReentrant"],
   istanbulReporter: ["html", "json-summary", "lcov", "text"],
+  mocha: {
+    grep: "@skip-on-coverage", // Find everything with this tag
+    invert: true, // Run the grep's inverse set.
+  },
 };
