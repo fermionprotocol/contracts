2d2023a7f7bef17b4145a6bf73bbbc2beea9b7d9
0xlucian <96285542+0xlucian@users.noreply.github.com>
2025-03-14 21:03:24 +0200

    Reorganise folder structure. (#384)
    * feat: reorganize folder structure and small refactoring of contract naming
    
    * refactor: apply PR code suggestions
    
    Co-authored-by: Klemen <64400885+zajck@users.noreply.github.com>
    Co-authored-by: Ludovic Levalleux <levalleux_ludo@hotmail.com>
    
    ---------
    
    Co-authored-by: Klemen <64400885+zajck@users.noreply.github.com>
    Co-authored-by: Ludovic Levalleux <levalleux_ludo@hotmail.com>

--- contracts/protocol/libs/ContextLib.sol
+++ contracts/protocol/libs/ContextLib.sol
@@ -3,26 +3,6 @@ pragma solidity 0.8.24;
 
 import { FermionStorage } from "./Storage.sol";
 
-/**
- * @title Execution context
- *
- * @notice Provides the message sender
- */
-contract Context {
-    uint256 private constant ADDRESS_LENGTH = 20;
-
-    /**
-     * @notice Returns the message sender address.
-     *
-     * @dev Could be msg.sender or the message sender address from storage (in case of meta transaction).
-     *
-     * @return the message sender address
-     */
-    function _msgSender() internal view virtual returns (address) {
-        return ContextLib._msgSender();
-    }
-}
-
 /*
  * @title Execution context
  *
