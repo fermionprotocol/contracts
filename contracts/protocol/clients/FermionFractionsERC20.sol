1286dd23b22f387360c9d7a3eac92e29e990a1f7
zajck <klemen@impressieve.com>
2025-03-17 11:33:58 +0100

    Add ERC20Permit

--- contracts/protocol/clients/FermionFractionsERC20.sol
+++ contracts/protocol/clients/FermionFractionsERC20.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.24;
 
-import { ERC20Upgradeable } from "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
+import { ERC20PermitUpgradeable } from "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20PermitUpgradeable.sol";
 import { OwnableUpgradeable } from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import { Initializable } from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import { IFermionFNFTPriceManager } from "../interfaces/IFermionFNFTPriceManager.sol";
@@ -12,7 +12,7 @@ import { ERC2771ContextUpgradeable as ERC2771Context } from "@openzeppelin/contr
  * @dev Implementation of the Fermion Fractions ERC20 epoch token.
  * This implementation is designed to be used with minimal proxies (EIP-1167).
  */
-contract FermionFractionsERC20 is Initializable, ERC20Upgradeable, ERC2771Context, OwnableUpgradeable {
+contract FermionFractionsERC20 is Initializable, ERC20PermitUpgradeable, ERC2771Context, OwnableUpgradeable {
     /// @custom:oz-upgrades-unsafe-allow constructor
     constructor(address _fermionProtocol) ERC2771Context(_fermionProtocol) {
         _disableInitializers();
@@ -24,6 +24,7 @@ contract FermionFractionsERC20 is Initializable, ERC20Upgradeable, ERC2771Contex
      */
     function initialize(string memory name_, string memory symbol_, address owner_) external initializer {
         __ERC20_init(name_, symbol_);
+        __ERC20Permit_init(name_);
         __Ownable_init(owner_);
     }
 
