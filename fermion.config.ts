c027fde7dc76ce571f657b2e310ced859680547f
0xlucian <0xluciandev@gmail.com>
2025-04-15 14:03:10 +0300

    Base Sepolia v1.1.0-rc.1 upgrade

--- fermion.config.ts
+++ fermion.config.ts
@@ -83,7 +83,7 @@ const fermionConfig: FermionConfig = {
         openSeaConduitKey: "0x0000007b02230091a7ed01230072f7006a004d60a8d4e71d599b8104250f0000",
         openSeaSignedZone: "0x000056F7000000EcE9003ca63978907a00FFD100",
         openSeaZoneHash: ZeroHash, // ToDo: add the correct value
-        openSeaRecipient: ZeroAddress, // ToDo: add the correct value
+        openSeaRecipient: "0x0000a26b00c1F0DF003000390027140000fAa719",
       },
       strictAuthorizedTransferSecurityRegistry: "0xA000027A9B2802E1ddf7000061001e5c005A0000",
       wrappedNative: "0x7b79995e5f793A07Bc00c21412e50Ecae098E7f9",
@@ -95,7 +95,7 @@ const fermionConfig: FermionConfig = {
         openSeaConduitKey: "0x0000007b02230091a7ed01230072f7006a004d60a8d4e71d599b8104250f0000",
         openSeaSignedZone: "0x000056F7000000EcE9003ca63978907a00FFD100",
         openSeaZoneHash: ZeroHash, // ToDo: add the correct value
-        openSeaRecipient: ZeroAddress, // ToDo: add the correct value
+        openSeaRecipient: "0x0000a26b00c1F0DF003000390027140000fAa719",
       },
       strictAuthorizedTransferSecurityRegistry: "0xA000027A9B2802E1ddf7000061001e5c005A0000",
       wrappedNative: "0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270",
@@ -107,7 +107,7 @@ const fermionConfig: FermionConfig = {
         openSeaConduitKey: "0x0000007b02230091a7ed01230072f7006a004d60a8d4e71d599b8104250f0000",
         openSeaSignedZone: "0x000056F7000000EcE9003ca63978907a00FFD100",
         openSeaZoneHash: ZeroHash, // ToDo: add the correct value
-        openSeaRecipient: ZeroAddress, // ToDo: add the correct value
+        openSeaRecipient: "0x0000a26b00c1F0DF003000390027140000fAa719",
       },
       strictAuthorizedTransferSecurityRegistry: "0xA000027A9B2802E1ddf7000061001e5c005A0000",
       wrappedNative: "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2",
@@ -117,8 +117,9 @@ const fermionConfig: FermionConfig = {
         seaport: "0x0000000000000068F116a894984e2DB1123eB395",
         openSeaConduit: "0x1E0049783F008A0085193E00003D00cd54003c71",
         openSeaConduitKey: "0x0000007b02230091a7ed01230072f7006a004d60a8d4e71d599b8104250f0000",
+        openSeaSignedZone: "0x000056F7000000EcE9003ca63978907a00FFD100",
         openSeaZoneHash: ZeroHash, // ToDo: add the correct value
-        openSeaRecipient: ZeroAddress, // ToDo: add the correct value
+        openSeaRecipient: "0x0000a26b00c1F0DF003000390027140000fAa719",
       },
       strictAuthorizedTransferSecurityRegistry: "0xA000027A9B2802E1ddf7000061001e5c005A0000",
       wrappedNative: "0x4200000000000000000000000000000000000006",
@@ -140,7 +141,7 @@ const fermionConfig: FermionConfig = {
         openSeaConduit: "0x1E0049783F008A0085193E00003D00cd54003c71",
         openSeaConduitKey: "0x0000007b02230091a7ed01230072f7006a004d60a8d4e71d599b8104250f0000",
         openSeaZoneHash: ZeroHash, // ToDo: add the correct value
-        openSeaRecipient: ZeroAddress, // ToDo: add the correct value
+        openSeaRecipient: "0x0000a26b00c1F0DF003000390027140000fAa719",
       },
       strictAuthorizedTransferSecurityRegistry: "0xA000027A9B2802E1ddf7000061001e5c005A0000",
       wrappedNative: "0x4200000000000000000000000000000000000006",
