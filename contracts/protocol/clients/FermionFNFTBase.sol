50d1121566d3db99d0b55e68a94ae00157029da1
0xlucian <0xluciandev@gmail.com>
2025-05-05 18:45:31 +0300

    chore: fft-01c change emptyslot type to unit256

--- contracts/protocol/clients/FermionFNFTBase.sol
+++ contracts/protocol/clients/FermionFNFTBase.sol
@@ -16,7 +16,7 @@ abstract contract FermionFNFTBase is ERC721, ERC2771Context {
     address internal immutable FERMION_PROTOCOL;
     address internal immutable BP_PRICE_DISCOVERY; // Boson protocol Price Discovery client
 
-    address internal emptySlot; // former fermionProtocol. Keep it to keep `voucherAddress` in slot 1.
+    uint256 internal emptySlot; // former fermionProtocol. Keep it to keep `voucherAddress` in slot 1.
 
     /**
      * @notice Constructor
