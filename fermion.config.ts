c385611c089fcf1efa388bffbc70a96ea738b709
0xlucian <0xluciandev@gmail.com>
2025-05-06 10:14:17 +0300

    Base v1.1.0-rc.1 upgrade

--- fermion.config.ts
+++ fermion.config.ts
@@ -129,8 +129,9 @@ const fermionConfig: FermionConfig = {
         seaport: "0x0000000000000068F116a894984e2DB1123eB395",
         openSeaConduit: "0x1E0049783F008A0085193E00003D00cd54003c71",
         openSeaConduitKey: "0x0000007b02230091a7ed01230072f7006a004d60a8d4e71d599b8104250f0000",
-        openSeaZoneHash: ZeroHash, // ToDo: add the correct value
-        openSeaRecipient: ZeroAddress, // ToDo: add the correct value
+        openSeaSignedZone: "0x000056F7000000EcE9003ca63978907a00FFD100",
+        openSeaZoneHash: ZeroHash,
+        openSeaRecipient: "0x0000a26b00c1F0DF003000390027140000fAa719",
       },
       strictAuthorizedTransferSecurityRegistry: "0xA000027A9B2802E1ddf7000061001e5c005A0000",
       wrappedNative: "0x4200000000000000000000000000000000000006",
